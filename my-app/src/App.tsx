a83d772d4edf65a77619a3afe3960ee19ea21514
EmmaRubiH <emmahjorting89@gmail.com>
2023-01-25 18:57:58 +0000

    adding styling på index.css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loading from './components/Loading';
